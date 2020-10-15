2269bd7e3940ec031f47f13a222b7740377cdee5
Mario-junior95 <karehmario@gmail.com>
2020-10-15 14:25:49 +0300

    Weather API: Getting the LIVE weather data

--- src/App.js
+++ src/App.js
@@ -9,16 +9,21 @@ import clear from "../src/img/weather-icons/clear.svg";
 import "./App.css";
 
 class Headers extends Component{
-  
+  state = {
+    input:""
+  };
   render(){
     var name = this.props.name;
 
     return (
         <div className = "header">
-        <form>
-          <input type="text" placeholder="Type a city name"></input>
-          <button><a href="javascript:;">{name}</a></button>
-        </form>
+          <input type="text" placeholder="Type a city name"  onChange={event => {
+            this.setState({ input: event.target.value });
+          }} />
+          <button  onClick={event => {
+            this.props.handleInput(this.state.input);
+          }}><a href="#">{name}</a></button>
+            {this.props.weather}
       </div>
       );
   }
@@ -101,23 +106,103 @@ class Weather_list extends Component{
     );
   }
 }
-
+// http:api.openweathermap.org/data/2.5/forecast?q=London&cnt=8&units=metric&appid=f5c2243aa45751703de79c648dd59473
+var name="London";
 class App extends Component {
-  render() {
-    var list = fakeWeatherData.list;
-    for(var i = 0;i<list.length;i++){
-      list[i].main.temp_max-=273.15;
-      list[i].main.temp_min-=273.15;
-      list[i].main.temp-=273.15;
-      list[i].main.temp = Math.round(list[i].main.temp);
-      list[i].main.temp_min = Math.round(list[i].main.temp_min);
-      list[i].main.temp_max = Math.round(list[i].main.temp_max);
-
+  constructor(props){
+    super(props);
+    this.state = {
+      role:undefined
     }
+  }
+  handleInputChange = value => {
+    this.changeWeather(value);
+
+
+  };
+  changeWeather(e){
+    name = e;
+    fetch("http://api.openweathermap.org/data/2.5/forecast?q="+e+"&cnt=8&units=metric&appid=f5c2243aa45751703de79c648dd59473")
+    .then(res => res.json())
+    .then(
+      (result) => {
+        this.setState({
+          role:result,
+          isLoaded: true,
+        });
+
+      },
+      (error) => {
+        this.setState({
+          isLoaded:true,
+          error
+        });
+      }
+    );
+  }
+  componentDidMount(){
+    
+    fetch("http://api.openweathermap.org/data/2.5/forecast?q="+name+"&cnt=8&units=metric&appid=f5c2243aa45751703de79c648dd59473")
+    .then(res => res.json())
+    .then(
+      (result) => {
+        this.setState({
+          role:result,
+          isLoaded: true,
+        });
+
+      },
+      (error) => {
+        this.setState({
+          isLoaded:true
+        });
+      }
+    );
+  }
+  render() {
+
+    var {isLoaded} =this.state;
+      if(isLoaded){
+        alert(name);
+
+        var x = this.state.role;
+        var list=x.list;
+
+
+
+
+
+      console.log(list);
+      var cur;
+      var c = [];
+
+      var id = list[0].weather[0].id;
+
+      for(var i=0;i<list.length;i++){
+        console.log(list[i].main.temp);
+
+        list[i].main.temp = Math.round(list[i].main.temp);
+        list[i].main.temp_min = Math.round(list[i].main.temp_min);
+        list[i].main.temp_max = Math.round(list[i].main.temp_max);
+          c.push(list[i]);
+
+
+
+      }
+
+      function sortf(a,b){
+        return a.dt_txt.localeCompare(b.dt_txt);
+      }
+      c.sort(sortf);
+  }
+  else{
+    return <div>Loading...</div>
+  }
+
     console.log(list);
      return (
        <div className="app" >
-         <Headers name="FIND WEATHER"/>
+         <Headers name="FIND WEATHER" handleInput = {this.handleInputChange}/>
          <div className="main">
            <CurrentWeather current = {list[0]}/>
            <Weather_list list = {list}/>
@@ -126,5 +211,5 @@ class App extends Component {
      );
    }
  }
- 
+
 export default App;
